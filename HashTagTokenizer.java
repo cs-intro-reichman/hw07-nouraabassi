8da741138d036b0ccd598574ef925b661b4de11e
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-02-08 09:20:45 +0200

    Update HashTagTokenizer.java

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -8,7 +8,6 @@ public class HashTagTokenizer {
 		//breakHashTag(hashTag, dictionary);
 	}
 
-	
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
@@ -20,16 +19,37 @@ public class HashTagTokenizer {
 			dictionary[n] = str;
 			n++;
 		}
-		return dictionary; }
+		return dictionary;
+	}
 
-	
 	public static boolean existInDictionary(String word, String []dictionary) {
 		for (int i = 0; i < 3000; i++) {
 			if (dictionary[i].equals(word)) {
 				return true;
-			}}
+			}
+		}
 		return false;
 	}
+
+	public static void breakHashTag(String hashtag, String[] dictionary) {
+
+		// Base case: do nothing (return) if hashtag is an empty string.
+        if (hashtag.isEmpty()) {
+            return;
+        }
+		hashtag = hashtag.toLowerCase();
+        int N = hashtag.length();
+
+        for (int i = 1; i <= N; i++) {
+			if (existInDictionary(hashtag.substring(0,i),dictionary)) {
+				System.out.println(hashtag.substring(0,i));
+				breakHashTag(hashtag.substring(i,N),dictionary);
+				break;
+			}
+        }
+    }
+
+}
 
 	
 	
